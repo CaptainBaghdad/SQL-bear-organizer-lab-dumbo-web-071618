66e14a71a5430fa79ea1f65ee9d94307b5a7449d
Jason Pitts <juniorjr083@gmail.com>
2018-07-24 00:32:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,6 +26,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   "Write your SQL query here"
+  "SELECT color, COUNT(color) FROM bears WHERE color = 'dark brown'";
 end
 
 def counts_number_of_bears_with_goofy_temperaments
